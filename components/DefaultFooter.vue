78c6b18adb6932ab1f4dc91de4d4ad95d0b77677
nhaimh <luongngochai29@gmail.com>
2023-03-09 14:40:45 +0700

    first commit

---
+++ components/DefaultFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="text_center">
+      <p>Design by <a href="fb.com/nhaimh">Lương Ngọc Hải</a></p>
+      <p>2023</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieAppFooter',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.text_center {
+  text-align: center !important;
+  height: 100px;
+  p {
+    color: white;
+    a {
+      text-decoration: none;
+      color: white;
+    }
+  }
+}
+</style>
